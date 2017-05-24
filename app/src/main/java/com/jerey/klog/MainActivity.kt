c5f151aaa0c24bc82e37c5e6ebbe600e35cb9cb9
Jerey-Jobs <610315802@qq.com>
2017-05-24 11:24:44 +0800

    修改使用demo

--- app/src/main/java/com/jerey/klog/MainActivity.kt
+++ app/src/main/java/com/jerey/klog/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.jerey.klog
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import com.jerey.loglib.KLog
 import com.jerey.loglib.log
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var s = "nihao"
-        s.log<String>()
-         .toUpperCase()
-         .log<String>()
-         .toLowerCase()
-         .log<String>()
+        var str = "nihao"
+        str.log()
+           .toUpperCase()
+           .log("upper")
+           .toLowerCase()
+           .log("lower")
+
+        KLog.getSettings()
+            .setBorderEnable(true)
+
+        KLog.a("aaaaaaa")
+        KLog.a(contents = "bbbbb")
+
+        var list = arrayListOf<String>("aaa","bb","cccc", "ddddd")
+        list.log("init")
+                .map { it -> it.toUpperCase() }
+                .log("after map")
+                .filter { it -> it.length > 2 }
+                .log("after filter")
+
 
     }
 }
